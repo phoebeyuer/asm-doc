afb1f394714a2fe59653b4aa383f9bd8b42c8578
phoebeyuer <yinyuefengshijie@qq.com>
2022-04-17 12:13:54 +0800

    test lint-staged

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,16 @@
 export default function baseStaticUrl(src = '') {
   const { VITE_APP_STATIC_URL } = import.meta.env
   if (src) {
-      return `${VITE_APP_STATIC_URL}${src}`;
+    return `${VITE_APP_STATIC_URL}${src}`
   }
-  return VITE_APP_STATIC_URL as string;
+  // "pre-commit": "lint-staged"
+  // hooksPath = .husky
+  // "lint-staged": {
+  //   "*.{ts,tsx,vue,js,jsx}": [
+  // 		"npm run lint",
+  //     "prettier --write",
+  //     "git add"
+  //   ]
+  // }
+  return VITE_APP_STATIC_URL as string
 }
